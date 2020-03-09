5a2b46f786541e816167c174b66f39ccadb6253b
hu-08 <hu-08@192.168.0.76>
2020-03-09 14:12:08 +0900

    2020/03/09

---
+++ 20200309/src/com/human/ex/javaStart.java
@@ -0,0 +1,109 @@
+package com.human.ex;
+
+public class javaStart {
+
+	public static void main(String[] args) {
+		/*
+		int sum=0;
+		for(int i=2; i<=10; i++) {
+			sum=sum+i;
+		}
+		System.out.println(sum);
+		*/
+		
+		
+		/*
+		int sum=0;
+		int i=2;
+		while(i<=10) {
+			sum=sum+i;
+			i++;
+		}
+		System.out.println(sum);
+		*/
+		
+		
+		/*
+		int sum=0;
+		int i=2;
+		do {
+			sum=sum+i;
+			i++;
+		}while(i<=10);
+		System.out.println(sum);
+		*/
+		
+		/*for(int i=0;i<10;i++) {
+			if(i==5) {
+				break;
+			}
+			System.out.println(i);
+		}
+		for(int i=0;i<10;i++) {
+			if(i==5) {
+				continue;
+			}
+			System.out.println(i);
+		}
+		*/
+		
+		java.util.Scanner sc=new java.util.Scanner(System.in);
+		/*int num1=Integer.parseInt(sc.nextLine());
+		int num2=Integer.parseInt(sc.nextLine());
+		
+		String op=sc.nextLine();
+		
+		int result=0;
+		
+		switch(op) {
+			case "*":
+				result=num1*num2;
+				break;
+			case "-":
+				result=num1-num2;
+				break;
+			case "/":
+				result=num1/num2;
+				break;
+			case "+":
+				result=num1+num2;
+				break;
+			default:
+				break;
+		}
+		
+		
+		
+		
+		
+		System.out.println(result);*/
+		
+		
+		for(int i=1;i<100;i++) {
+			if(i%4==0) {
+				continue;
+			}
+			System.out.println(i);
+		}
+
+		
+		for(int i=1;i<100;i++) {
+			if(i%4==0) {
+				continue;
+			}
+			System.out.println(i);
+		}
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+
+}
